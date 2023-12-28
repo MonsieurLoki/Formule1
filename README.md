34e4cb14a8056bd639236c76d1e103d703d1b60a
gaspard <gaspard@example.com>
2023-12-28 22:24:17 +0100

    gestion des meilleurs secteurs et du status

--- README.md
+++ README.md
@@ -12,26 +12,61 @@ OS2 - TP1 : Formula1 championship.
 - [Installation](#installation)
 - [Usage](#usage)
 - [Contributing](#contributing)
-- [License](#license)
 
 ## Introduction
 
-Explain what the project is about, 
+This is a group project from Ephec students in TI2, OS2
 
 ## Features
 
-- List of main features or functionalities.
-- You can use bullet points or a descriptive list.
+- Simulate a Formula1 race
+- Max 22 cars; 20 different races across the season
+- Manage a full season championship
+- Real time display of the race statistics, with graphical representation
 
 ## Installation
 
 
-To install :
+To install (first time):
 
 ```bash
 # Sample installation steps
 git clone https://github.com/MonsieurLoki/Formule1.git
 cd Formule1
 gcc cars.c -o cars
-./cars
+```
 
+
+To update the code :
+
+```bash
+# Sample installation steps
+# cd ../Formule1
+git pull 
+gcc cars.c -o cars
+```
+
+
+## Usage
+
+```bash
+./cars
+...
+176) voiture  0, secteur courant : 3, position :   42 
+176) voiture  1, secteur courant : 2, position :   72 
+176) voiture  2, secteur courant : 2, position :   56 
+176) voiture  3, secteur courant : 2, position :   95 
+  0) voiture sect: 3, pos: 42, tour:1 -----------------------------|-----------------------------|------------*----------------|
+  1) voiture sect: 2, pos: 72, tour:1 -----------------------------|--------------------*--------|-----------------------------|
+  2) voiture sect: 2, pos: 56, tour:1 -----------------------------|----------------*------------|-----------------------------|
+  3) voiture sect: 2, pos: 95, tour:1 -----------------------------|---------------------------*-|-----------------------------|
+  ...
+```
+
+## Contributing
+
+Ephec Students
+- Gaspard
+- Toto
+- Tutu
+- Titi
--- cars.c
+++ cars.c
@@ -17,7 +17,11 @@
 
 #define IPC_RESULT_ERROR (-1)
 #define NB_VOITURE 4
-#define NB_TOUR 3
+#define NB_TOUR 2
+#define STATUS_FINISHED 2
+#define STATUS_RUNNING 1
+#define STATUS_READY 0
+
 #ifndef SHARED_MEMORY_H
 #define SHARED_MEMORY_H
 
@@ -38,27 +42,33 @@ struct Car
 {
     int car_nr;
     char pilote[20];
-    float best_time_sector1;
-    float best_time_sector2;
-    float best_time_sector3;
+    float time_sector1;
+    float time_sector2;
+    float time_sector3;
     int position;
     int secteur_courant;
     int tour_courant;
+    int best_tour_time_ms;
+    int total_time_de_la_course_ms;
+    int status;
 };
+
+char *char_status_desc[] = {"ready ", "Running", "Finished"};
 
 // definition of the Best_sector times
 struct Best_sector
 {
     // int time;      // best time for this sector in secs
-    int time_ms;   // best time for this sector in ms
-    int car_index; // car having done the best time for this sector
+    int time_ms; // best time for this sector in ms
+    int car_nr;  // car having done the best time for this sector
 };
 
 // definition of the structure of the shared memmory
+// malgré que trois secteurs on créer un tableau avec quatres lignes pour simplifier la programmation
 typedef struct
 {
     struct Car cars[NB_VOITURE + 1];
-    struct Best_sector best_sectors[3];
+    struct Best_sector best_sectors[4];
 } Shmem_data;
 
 Shmem_data *shmem_data;
@@ -129,40 +139,59 @@ int myRandom(int min, int max)
     return nombreAlea;
 }
 
-void gererSecteur(int car_nr, int nr_secteur)
+// cette fonction gère le secteur nr_secteur de la voiture car_nr et retourne le temps en millisecondes que la voiture a pris pour parcourir le secteur
+int gererSecteur(int car_nr, int nr_secteur)
 {
-    int dureeCourse_ms = myRandom(7000, 12000);
+    int dureeSecteur_ms = myRandom(4000, 6000);
     int passed_ms = 0; // nb of ms passed since beginning of the sector
     int delay_ms = 100;
     int position;
     shmem_data->cars[car_nr].secteur_courant = nr_secteur;
-    while (passed_ms <= dureeCourse_ms)
+    while (passed_ms <= dureeSecteur_ms)
     {
-        position = ((100 * passed_ms) / dureeCourse_ms);
+        position = ((100 * passed_ms) / dureeSecteur_ms);
         shmem_data->cars[car_nr].position = position;
         usleep(delay_ms * 1000); // attendre x microsecond
         passed_ms += delay_ms;
     }
     shmem_data->cars[car_nr].position = 100;
+    // shmem_data->cars[car_nr].time_sector1 = dureeSecteur_ms;
+    switch (nr_secteur)
+    {
+    case 1:
+        shmem_data->cars[car_nr].time_sector1 = dureeSecteur_ms;
+        break;
+    case 2:
+        shmem_data->cars[car_nr].time_sector2 = dureeSecteur_ms;
+        break;
+    case 3:
+        shmem_data->cars[car_nr].time_sector3 = dureeSecteur_ms;
+        break;
+    default:
+        printf("Erreur dans switch(nr_secteur) : ceci ne devrait jamais arriver !");
+        exit(1);
+        break;
+    }
+
+    if (dureeSecteur_ms < shmem_data->best_sectors[nr_secteur].time_ms || shmem_data->best_sectors[nr_secteur].time_ms == 0)
+    {
+        shmem_data->best_sectors[nr_secteur].time_ms = dureeSecteur_ms;
+        shmem_data->best_sectors[nr_secteur].car_nr = car_nr;
+    }
+    return dureeSecteur_ms;
 }
 
 void dessiner(int car_nr, int position)
 {
+    int status = shmem_data->cars[car_nr].status;
     int tour_courant = shmem_data->cars[car_nr].tour_courant;
     int secteur_courant = shmem_data->cars[car_nr].secteur_courant;
     const int nombreTirets = 30;
-    char secteur_0[nombreTirets + 1];
     char secteur_1[nombreTirets + 1];
     char secteur_2[nombreTirets + 1];
+    char secteur_3[nombreTirets + 1];
 
     // fill sectors
-    for (int i = 0; i < nombreTirets; i++)
-    {
-        secteur_0[i] = '-';
-    }
-    secteur_0[nombreTirets] = '\0';
-    secteur_0[nombreTirets - 1] = '|';
-
     for (int i = 0; i < nombreTirets; i++)
     {
         secteur_1[i] = '-';
@@ -177,12 +206,15 @@ void dessiner(int car_nr, int position)
     secteur_2[nombreTirets] = '\0';
     secteur_2[nombreTirets - 1] = '|';
 
+    for (int i = 0; i < nombreTirets; i++)
+    {
+        secteur_3[i] = '-';
+    }
+    secteur_3[nombreTirets] = '\0';
+    secteur_3[nombreTirets - 1] = '|';
+
     // compute where to put the car
     int positionEtoile = (nombreTirets - 1) * position / 100;
-    if (secteur_courant == 0)
-    {
-        secteur_0[positionEtoile] = '*';
-    }
     if (secteur_courant == 1)
     {
         secteur_1[positionEtoile] = '*';
@@ -191,36 +223,22 @@ void dessiner(int car_nr, int position)
     {
         secteur_2[positionEtoile] = '*';
     }
+    if (secteur_courant == 3)
+    {
+        secteur_3[positionEtoile] = '*';
+    }
 
-    printf("%3d) voiture sect:%2d, pos:%3d, tour:%d ", car_nr, secteur_courant, position, tour_courant);
+    printf("%3d) voiture, status: %s, sect:%2d, pos:%3d, tour:%d ", car_nr, char_status_desc[status], secteur_courant, position, tour_courant);
 
-    printf("%s%s%s\n", secteur_0, secteur_1, secteur_2);
+    printf("%s%s%s\n", secteur_1, secteur_2, secteur_3);
 }
-
-// prend la valeur a la fin de la chaine de caractère après la |
-// int getValue(char *chaine)
-// {
-//     // Chercher la position du caractère '|'
-//     char *separator = strchr(chaine, '|');
-
-//     if (separator != NULL)
-//     {
-//         // Utiliser strtok pour diviser la chaîne à partir de '|'
-//         char *token = strtok(separator, "|");
-
-//         // Utiliser atoi pour convertir le token suivant '|' en entier
-//         if (token != NULL)
-//         {
-//             return atoi(token);
-//         }
-//     }
-
-//     return -1; // Valeur par défaut si le symbole '|' n'est pas trouvé ou s'il n'y a pas de valeur après '|'
-// }
 
 void vivre_ma_vie_de_voiture(int car_nr)
 {
+    int temps_tour_courant_ms;
     char *block;
+    int tour_courant = 0;
+    int temps_total_ms = 0;
     block = attach_memory_block(FILENAME, BLOCK_SIZE);
     if (block == NULL)
     {
@@ -229,20 +247,24 @@ void vivre_ma_vie_de_voiture(int car_nr)
     }
 
     shmem_data = (Shmem_data *)block;
-    int tour_courant = 0;
     srand(time(NULL) + car_nr);
+    shmem_data->cars[car_nr].status = STATUS_RUNNING;
     while (tour_courant < NB_TOUR)
     {
         tour_courant += 1;
         shmem_data->cars[car_nr].tour_courant = tour_courant;
-        gererSecteur(car_nr, 0);
-        gererSecteur(car_nr, 1);
-        gererSecteur(car_nr, 2);
+        temps_tour_courant_ms = 0;
+        temps_tour_courant_ms += gererSecteur(car_nr, 1);
+        temps_tour_courant_ms += gererSecteur(car_nr, 2);
+        temps_tour_courant_ms += gererSecteur(car_nr, 3);
+        if (temps_tour_courant_ms < shmem_data->cars[car_nr].best_tour_time_ms || tour_courant == 1)
+        {
+            shmem_data->cars[car_nr].best_tour_time_ms = temps_tour_courant_ms;
+        }
+        temps_total_ms += temps_tour_courant_ms;
+        shmem_data->cars[car_nr].total_time_de_la_course_ms = temps_total_ms;
     }
-
-    // int i = 0;
-    // int delay = 1;
-
+    shmem_data->cars[car_nr].status = STATUS_FINISHED;
     detach_memory_block(block);
 }
 
@@ -252,18 +274,14 @@ int toutesLesVoituresOntTermine()
 {
     for (int i = 0; i < NB_VOITURE; i++)
     {
-        if (shmem_data->cars[i].position == 100)
-        {
-        }
-        else
+        if (shmem_data->cars[i].position < 100)
         {
             return 0;
         }
     }
     return 1;
 }
-
-void gererVoitures()
+void monitorerVoitures()
 {
     int iter = 0;
     while (1)
@@ -273,7 +291,15 @@ void gererVoitures()
 
         for (int i = 0; i < NB_VOITURE; i++)
         {
-            printf("%3d) voiture %2d, secteur courant : %d, position :  %3d \n", iter, i, shmem_data->cars[i].secteur_courant, shmem_data->cars[i].position);
+            int secteur_courant = shmem_data->cars[i].secteur_courant;
+            int position = shmem_data->cars[i].position;
+            int time_sector1 = shmem_data->cars[i].time_sector1;
+            int time_sector2 = shmem_data->cars[i].time_sector2;
+            int time_sector3 = shmem_data->cars[i].time_sector3;
+            int best_tour_time = shmem_data->cars[i].best_tour_time_ms;
+            int temps_total = shmem_data->cars[i].total_time_de_la_course_ms;
+            printf("%3d) voiture %2d, secteur courant : %d, temps secteur1 : %6.3f, temps secteur2 : %6.3f, temps secteur3 : %6.3f, best tour time : %6.3f, temps total : %6.3f \n",
+                   iter, i, secteur_courant, (time_sector1 * 1.0) / 1000, (time_sector2 * 1.0) / 1000, (time_sector3 * 1.0) / 1000, (best_tour_time * 1.0) / 1000, (temps_total * 1.0) / 1000);
         }
 
         for (int i = 0; i < NB_VOITURE; i++)
@@ -281,6 +307,9 @@ void gererVoitures()
             dessiner(i, shmem_data->cars[i].position);
         }
 
+        printf("best time for sector 1: time : %6.3f, car : %d \n", (shmem_data->best_sectors[1].time_ms * 1.0) / 1000, shmem_data->best_sectors[1].car_nr);
+        printf("best time for sector 2: time : %6.3f, car : %d \n", (shmem_data->best_sectors[2].time_ms * 1.0) / 1000, shmem_data->best_sectors[2].car_nr);
+        printf("best time for sector 3: time : %6.3f, car : %d \n", (shmem_data->best_sectors[3].time_ms * 1.0) / 1000, shmem_data->best_sectors[3].car_nr);
         if (toutesLesVoituresOntTermine() == 1)
         {
             break;
@@ -289,10 +318,25 @@ void gererVoitures()
     }
 }
 
+// initialisation de la mémoire partagée pour être sur de ne pas les afficher avant initialisation
+void initialiser_shm_pour_voiture(int nr_voiture)
+{
+    shmem_data->cars[nr_voiture].best_tour_time_ms = 0;
+    shmem_data->cars[nr_voiture].time_sector1 = 0;
+    shmem_data->cars[nr_voiture].time_sector2 = 0;
+    shmem_data->cars[nr_voiture].time_sector3 = 0;
+    shmem_data->cars[nr_voiture].secteur_courant = 0;
+    shmem_data->cars[nr_voiture].position = 0;
+    shmem_data->cars[nr_voiture].tour_courant = 0;
+    shmem_data->cars[nr_voiture].status = STATUS_READY;
+}
+
 int lancerVoiture(int nr_voiture)
 {
     int pid;
     pid = fork();
+
+    initialiser_shm_pour_voiture(nr_voiture);
 
     if (pid == -1)
     {
@@ -311,6 +355,16 @@ int lancerVoiture(int nr_voiture)
     }
 }
 
+void initialisation_best_sectors_time()
+{
+    shmem_data->best_sectors[1].car_nr = 0;
+    shmem_data->best_sectors[1].time_ms = 0;
+    shmem_data->best_sectors[2].car_nr = 0;
+    shmem_data->best_sectors[2].time_ms = 0;
+    shmem_data->best_sectors[3].car_nr = 0;
+    shmem_data->best_sectors[3].time_ms = 0;
+}
+
 int main()
 {
     destroy_shmem();
@@ -324,13 +378,13 @@ int main()
 
     // mapping the Shmem_data structure on the shared memory block
     shmem_data = (Shmem_data *)block;
-
+    initialisation_best_sectors_time();
     pid_t pid[NB_VOITURE];
     for (int i = 0; i < NB_VOITURE; i++)
     {
         pid[i] = lancerVoiture(i);
     }
-    gererVoitures();
+    monitorerVoitures();
     detach_memory_block(block);
 
     destroy_shmem();
